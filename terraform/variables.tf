499864de8e36d4e353e6fb166ea6d458985f56f6
Dragan Gojic <gojic37@gmail.com>
2025-10-20 18:49:00 +0200

    initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "name" {
+  type        = string
+  description = "Basisname für Ressourcen"
+  default     = "meteo-staging"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure Region"
+  default     = "westeurope"
+}
+
+variable "vm_size" {
+  type        = string
+  description = "VM SKU"
+  default     = "Standard_B1s"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Linux Admin-User"
+  default     = "azureuser"
+}
+
+variable "ssh_public_key_data" {
+  type        = string
+  description = "SSH Public Key (wird im Workflow ephemer erzeugt)"
+}
