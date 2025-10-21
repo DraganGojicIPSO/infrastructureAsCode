ac9105cba41a61a6fcce6621aa2e554f6e50740c
Dragan Gojic <gojic37@gmail.com>
2025-10-22 00:03:24 +0200

    support tls

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,6 +77,18 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "https"
+    priority                   = 111
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = local.tags
 }
 
